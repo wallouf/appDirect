44c7d382442645a58e1413f4604115071502f46f
wallouf <ct13860@gmail.com>
2017-07-30 18:10:15 -0400

    prepare aws and change port to 80

--- AUTO_INSTALL.sh
+++ AUTO_INSTALL.sh
@@ -9,6 +9,15 @@ var_stepChoice=0
 var_interactionChoice=1
 var_imageName="wallouf-appdirect-application"
 var_dockerIdUser="wallouf"
+#AWS PARAMS
+var_amiId="ami-a7aa15c3"
+var_computeType="t2.micro"
+var_sshKeyName="BOC-SSH-KEY"
+var_subnetId="subnet-4434c13f"
+var_securityGroupId="sg-7688df1f"
+
+#DO NOT CHANGE
+var_instanceId=""
 
 function print_usage() {
 	clear
@@ -17,6 +26,7 @@ function print_usage() {
 	echo "AUTO INSTALL SCRIPT"
 	echo 
 	echo "	WARNING:"
+	echo "		- You need to have python > 2.7 at least to deploy"
 	echo "		- You need to have apache maven > 3.5 and java 1.8 at least to build"
 	echo "		- You need a docker account and a docker repository with the following name: ${var_imageName}"
 	echo "		- Before using this script, you need to set up your credentials into the CREDENTIALS.properties file."
@@ -27,6 +37,8 @@ function print_usage() {
 	echo "	2 - Build docker image"
 	echo "	3 - Push docker image"
 	echo "	4 - Deploy image in AWS"
+	echo "	[Optional]"
+	echo "	c - Deploy image in AWS Cloud"
 	echo 
 	echo "	USAGE:"
 	echo 
@@ -44,8 +56,20 @@ function print_usage() {
     echo
     echo "  -p,--push-docker-only		Push only with docker the application image."
     echo
-    echo "  -i,--install-only			Install only with AWS the application image."
+    echo "  -i,--install-only			Install the application image on this computer."
     echo
+    echo "  -c,--cloud-only				Install only with AWS the application image."
+    echo
+}
+
+function stopAWS_EC2(){
+	aws ec2 terminate-instances --instance-ids ${var_instanceId}
+	if [ $? -ne 0 ]; then
+	    echo "${boldRedEchoStyle}        Termination of EC2 ${var_instanceId} failed.${resetEchoStyle}"
+	    return -1
+	else
+		return 0
+	fi
 }
 
 print_usage
@@ -69,6 +93,10 @@ while :; do
             ;;
         -i|--install-only)
             var_stepChoice=4
+            var_interactionChoice=0
+            ;;
+        -c|--cloud-only)
+            var_stepChoice=5
             var_interactionChoice=0
             ;;
 
@@ -203,7 +231,7 @@ fi
 #STEP 4
 if [[ ${var_stepChoice} -eq 0 ]] || [[ ${var_stepChoice} -eq 4 ]]; then
 	echo
-    echo "${boldOrangeEchoStyle}	-> Step 4: Install with AWS...${resetEchoStyle}"
+    echo "${boldOrangeEchoStyle}	-> Step 4: Install locally...${resetEchoStyle}"
 
     sudo service docker start
 	if [ $? -ne 0 ]; then
@@ -211,13 +239,128 @@ if [[ ${var_stepChoice} -eq 0 ]] || [[ ${var_stepChoice} -eq 4 ]]; then
 	    exit -1
 	fi
 
-    sudo docker run -d -p 8080:8080 ${var_imageName}
+    sudo docker run -d -p 80:80 ${var_imageName}
 	if [ $? -ne 0 ]; then
 	    echo "${boldRedEchoStyle}        Deployment failed with docker. Please fix project and try again.${resetEchoStyle}"
 	    exit -1
 	fi
 
-    echo "${boldGreenEchoStyle}	-> Step 4: Install with AWS DONE!${resetEchoStyle}"
+    echo "${boldGreenEchoStyle}	-> Step 4: Install locally DONE!${resetEchoStyle}"
+fi
+
+
+#STEP 5 - CLOUD
+if [[ ${var_stepChoice} -eq 0 ]] || [[ ${var_stepChoice} -eq 5 ]]; then
+	echo
+    echo "${boldOrangeEchoStyle}	-> Step Cloud: Install with AWS...${resetEchoStyle}"
+
+    #INSTALL AWS CLI
+    pip install awscli --upgrade --user
+	if [ $? -ne 0 ]; then
+	    echo "${boldRedEchoStyle}        Installation of AWS CLI failed. Please try again.${resetEchoStyle}"
+	    exit -1
+	fi
+
+	echo "export PATH=~/.local/bin:$PATH" >> ~/.profile
+
+	source ~/.profile
+
+	aws --version
+	if [ $? -ne 0 ]; then
+	    echo "${boldRedEchoStyle}        Installation of AWS CLI failed. Please try again.${resetEchoStyle}"
+	    exit -1
+	fi
+
+	aws configure
+	if [ $? -ne 0 ]; then
+	    echo "${boldRedEchoStyle}        Configuration of AWS CLI failed. Please try again.${resetEchoStyle}"
+	    exit -1
+	fi
+
+	varNameTag=${var_imageName}-$(date +%s)
+
+	aws ec2 run-instances --image-id ${var_amiId} --count 1 --instance-type ${var_computeType} --key-name ${var_sshKeyName} --security-group-ids ${var_securityGroupId} --subnet-id ${var_subnetId} --tags Key=Name,Value=${varNameTag} > ec2_creation.log
+	if [ $? -ne 0 ]; then
+	    echo "${boldRedEchoStyle}        Creation of EC2 failed. Please try again.${resetEchoStyle}"
+	    exit -1
+	fi
+
+	var_instanceId=$(grep -Po '"InstanceId":.*?[^\\]",' ec2_creation.log)
+	var_instanceId=${var_instanceId#\"InstanceId\": \"}
+	var_instanceId=${var_instanceId%\",}
+	if [ ! "${var_instanceId}" ]; then
+	    echo "${boldRedEchoStyle}        Cannot get id of EC2 instance. Please terminate it manually and try again.${resetEchoStyle}"
+		exit -1
+	fi
+	#RETRIEVE PUBLIC IP
+	var_try=0
+	var_instanceIP=$(aws ec2 describe-instances --instance-ids ${var_instanceId} --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
+
+	while [ ${var_try} -lt 30 ] && [ ! "${var_instanceIP}" ]
+	do
+	   var_try=$((var_try+1))
+	   sleep 1
+		var_instanceIP=$(aws ec2 describe-instances --instance-ids ${var_instanceId} --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
+	done
+
+	if [ ! "${var_instanceIP}" ]; then
+		varResult=stopAWS_EC2
+		if [ $varResult -eq 0 ]; then
+		    echo "${boldRedEchoStyle}        Cannot get public ip of EC2 instance. Please try again.${resetEchoStyle}"
+		else
+		    echo "${boldRedEchoStyle}        Cannot get public ip of EC2 instance. Termination of instance failed. Please close it manually and try again.${resetEchoStyle}"
+		fi
+		exit -1
+	fi
+
+	chmod 600 ${var_sshKeyName}.pem
+
+	ssh -i ${var_sshKeyName}.pem ec2-user@${var_instanceIP}
+	if [ $? -ne 0 ]; then
+		varResult=stopAWS_EC2
+		if [ $varResult -eq 0 ]; then
+		    echo "${boldRedEchoStyle}        Connection to EC2 failed. Please try again.${resetEchoStyle}"
+		else
+		    echo "${boldRedEchoStyle}        Connection to EC2 failed. Termination of instance failed. Please close it manually and try again.${resetEchoStyle}"
+		fi
+		exit -1
+	fi
+
+	sudo yum install -y docker
+	if [ $? -ne 0 ]; then
+		varResult=stopAWS_EC2
+		if [ $varResult -eq 0 ]; then
+		    echo "${boldRedEchoStyle}        Docker installation failed. Please try again.${resetEchoStyle}"
+		else
+		    echo "${boldRedEchoStyle}        Docker installation failed. Termination of instance failed. Please close it manually and try again.${resetEchoStyle}"
+		fi
+		exit -1
+	fi
+
+	sudo service docker start
+	if [ $? -ne 0 ]; then
+		varResult=stopAWS_EC2
+		if [ $varResult -eq 0 ]; then
+		    echo "${boldRedEchoStyle}        Docker failed to start. Please try again.${resetEchoStyle}"
+		else
+		    echo "${boldRedEchoStyle}        Docker failed to start. Termination of instance failed. Please close it manually and try again.${resetEchoStyle}"
+		fi
+		exit -1
+	fi
+
+    sudo docker run -d -p 80:80 ${var_dockerIdUser}/${var_imageName}
+    if [ $? -ne 0 ]; then
+		varResult=stopAWS_EC2
+		if [ $varResult -eq 0 ]; then
+		    echo "${boldRedEchoStyle}        Application failed to start. Please try again.${resetEchoStyle}"
+		else
+		    echo "${boldRedEchoStyle}        Application failed to start. Termination of instance failed. Please close it manually and try again.${resetEchoStyle}"
+		fi
+		exit -1
+	fi
+
+
+    echo "${boldGreenEchoStyle}	-> Step Cloud: Install with AWS DONE! You can try your new application at the following url http://${var_instanceIP}${resetEchoStyle}"
 fi
 
 
